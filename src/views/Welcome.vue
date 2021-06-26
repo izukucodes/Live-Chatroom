b6090ccf9cfb4e05ca4dabb174227f2c7d593795
izukucodes <izukucodes@gmail.com>
2021-06-26 21:47:08 +0500

    init

---
+++ src/views/Welcome.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="welcome container">
+    Welcome
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.welcome {
+  text-align: center;
+  padding: 20px 0px;
+}
+</style>
