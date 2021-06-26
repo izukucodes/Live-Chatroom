5676f504d8adf28f2fcab63ad91ed28e5d652c49
izukucodes <izukucodes@gmail.com>
2021-06-26 22:16:53 +0500

    Added Signup & Login

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,25 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <label>Email</label>
+    <input type="text" v-model="email" />
+    <label>Password</label>
+    <input type="password" v-model="password" />
+    <button>Login</button>
+  </form>
+</template>
+
+<script>
+import { ref } from "@vue/reactivity";
+export default {
+  setup() {
+    const email = ref("");
+    const password = ref("");
+    const handleSubmit = () => {
+      console.log(email.value, password.value);
+    };
+    return { email, password, handleSubmit };
+  },
+};
+</script>
+
+<style></style>
---
+++ src/components/SignupForm.vue
@@ -0,0 +1,28 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <label>Name</label>
+    <input type="text" v-model="name" />
+    <label>Email</label>
+    <input type="text" v-model="email" />
+    <label>Password</label>
+    <input type="password" v-model="password" />
+    <button>Register</button>
+  </form>
+</template>
+
+<script>
+import { ref } from "@vue/reactivity";
+export default {
+  setup() {
+    const name = ref("");
+    const email = ref("");
+    const password = ref("");
+    const handleSubmit = () => {
+      console.log(name.value, email.value, password.value);
+    };
+    return { name, email, password, handleSubmit };
+  },
+};
+</script>
+
+<style></style>
